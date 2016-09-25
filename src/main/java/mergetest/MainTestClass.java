65c7d450d74cf4f2e74c68c5fc4ec42a87cb16bf
herbert scruggs <herbscruggs@gmail.com>
2016-09-24 20:06:00 -0500

    feature change from someone

--- src/main/java/mergetest/MainTestClass.java
+++ src/main/java/mergetest/MainTestClass.java
@@ -3,7 +3,6 @@ package mergetest;
 public class MainTestClass {
 
 
-    //Someone checked this in and we need to keep this from MAster
-    //This is the master change
+    //Moving along with a changes from developers
 
 }
