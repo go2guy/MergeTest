a7adc3367530679ff88fb4e643cafe9c6caa7a47
Herb Scruggs <herb.scruggs@newnet.com>
2017-04-13 13:32:53 -0500

    no message

--- src/main/java/mergetest/MainTestClass.java
+++ src/main/java/mergetest/MainTestClass.java
@@ -3,7 +3,7 @@ package mergetest;
 public class MainTestClass {
 
 
-
+Change in master
     //feature1 is moving along now
     //Moving along with a changes from developers
     //Okay made some more changes in master
