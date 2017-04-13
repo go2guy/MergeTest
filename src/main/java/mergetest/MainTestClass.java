8d9faac1e630b2cc5a28bee7108343aac6acc85e
Herb Scruggs <herb.scruggs@newnet.com>
2017-04-13 13:46:42 -0500

    C

--- src/main/java/mergetest/MainTestClass.java
+++ src/main/java/mergetest/MainTestClass.java
@@ -3,7 +3,7 @@ package mergetest;
 public class MainTestClass {
 
 
-
+TRy this again get this into the master on a merge
 I'm homeing along and making changes.
     //feature1 is moving along now
     //Moving along with a changes from developers
